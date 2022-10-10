10ae792bebf18843f9e511584f2e2e324fec0b51
exploitconsult1 <115463991+exploitconsult1@users.noreply.github.com>
2022-10-10 13:20:14 +0100

    Delete account_move_garantie directory

--- account_move_garantie/models/models.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class res_company_inherit(models.Model):
-    _inherit = 'res.company'
-    _description = 'res.company.inherit'
-
-    description = fields.Text(string="Description facture")
